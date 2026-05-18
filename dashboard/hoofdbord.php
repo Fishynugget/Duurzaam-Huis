55aaf0dd3201097c1c91ef7d6ca0555f2969849f
Fishynugget <HelenadeHaan235@gmail.com>
2026-05-18 12:11:49 +0200

    vakjes gemaakt

--- dashboard/hoofdbord.php
+++ dashboard/hoofdbord.php
@@ -8,6 +8,21 @@
 </head>
 <body>
     header met logo en titel
+    <table id="bord">
+        <?php
+        function vakjes() 
+        {
+            for ($column = 0; $column < 3; $column++) { 
+                echo "<tr>";
+                for ($row = 0; $row < 3; $row++) { 
+                    echo '<td id="include_vakjes"></td>';
+                }
+                echo "</tr>";
+            }
+        }
+        vakjes();
+        ?>
+    </table>
     footer met namen groepje
 </body>
 </html>
