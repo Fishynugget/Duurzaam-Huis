da365eab7abc5d0e5623459b3668c8ceada2ee5b
Fishynugget <HelenadeHaan235@gmail.com>
2026-05-18 11:42:25 +0200

    folders en files aangemaakt

---
+++ dashboard/hoofdbord.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="hoofdbord.css">
+    <title>Document</title>
+</head>
+<body>
+    header met logo en titel
+    footer met namen groepje
+</body>
+</html>
