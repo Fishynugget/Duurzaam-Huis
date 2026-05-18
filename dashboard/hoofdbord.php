bd38d00c4499631fa07daa0efc6b03d537a4ea77
Fishynugget <HelenadeHaan235@gmail.com>
2026-05-18 12:33:35 +0200

    frame aangepast

--- dashboard/hoofdbord.php
+++ dashboard/hoofdbord.php
@@ -1,28 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="hoofdbord.css">
     <title>Document</title>
 </head>
+
 <body>
-    header met logo en titel
-    <table id="bord">
-        <?php
-        function vakjes() 
-        {
-            for ($column = 0; $column < 3; $column++) { 
-                echo "<tr>";
-                for ($row = 0; $row < 3; $row++) { 
-                    echo '<td id="include_vakjes"></td>';
+    <header>
+        header met logo en titel
+    </header>
+    <main>
+        <table id="bord">
+            <?php
+            function vakjes()
+            {
+                for ($column = 0; $column < 3; $column++) {
+                    echo "<tr>";
+                    for ($row = 0; $row < 3; $row++) {
+                        echo '<td id="include_vakjes"></td>';
+                    }
+                    echo "</tr>";
                 }
-                echo "</tr>";
             }
-        }
-        vakjes();
-        ?>
-    </table>
-    footer met namen groepje
+            vakjes();
+            ?>
+        </table>
+    </main>
+    <footer>
+        footer met namen groepje
+    </footer>
 </body>
+
 </html>
